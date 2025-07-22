248fffebf10d47297fa5715c152eb584dfdd9ae1
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-07-22 07:34:02 +0530

    fixing deployment issue

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 app.use(cookieParser());
 
