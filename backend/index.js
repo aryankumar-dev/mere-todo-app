8a36e62a8f10ae3b1bf9a221ab9affae6bea235b
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-07-22 16:00:47 +0530

    fix: _redirects routing working for Vite on Render

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,9 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://your-frontend-url.onrender.com',
+    origin: 'https://mere-todo-app-1.onrender.com',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
-
 
 
 app.use(cookieParser());
