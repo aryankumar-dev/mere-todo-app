f0ca08c3c1174b6c08ac94833aa4f6cfacd4ef1d
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-07-22 08:19:56 +0530

    fixing deployment issueagain

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,15 @@ const app = express();
 
 
 
+app.options('*', (req, res) => {
+    res.set('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.sendStatus(200);
+});
+
+
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
@@ -30,9 +39,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/task', taskRoutes);
 
 // ✅ MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error(err));
+
 
 // ✅ Root test route
 app.get('/', (req, res) => res.send('API is working'));
